f4b359ec7e96507c3c2568470e2d4ca961765541
Eric Yu <chunkaiplus@gmail.com>
2016-06-12 23:05:25 +0800

    Add proxy server settings

--- generators/app/templates/server.js
+++ generators/app/templates/server.js
@@ -10,12 +10,10 @@ new WebpackDevServer(webpack(config), {
   stats: {
     colors: true
   },
-  proxy: {
-    "/papilio_api/*": "http://localhost:8888",
-    "/yp/*": "http://localhost:8888",
-    "/user_label/*": "http://localhost:8888",
-    "/simulation/*": "http://localhost:8888"
-  }
+  // If you need it
+  //proxy: {
+  //  "*": "http://localhost:8000"
+  //}
 }).listen(3000, 'localhost', function (err) {
   if (err) {
     console.log(err);
